f128dc19a62168b14435013ac9d84aa951cf6173
Matthew Cloutier <mrcloutier624@gmail.com>
2024-09-22 17:43:51 -0400

    WIP calculate

--- sources_1/new/Calculate.v
+++ sources_1/new/Calculate.v
@@ -21,6 +21,31 @@
 
 
 module Calculate(
-
+    input btnr,
+    input [2:0] sw,
+    input [2:0] state,
+    input [7:0] XOR,
+    input [7:0] AND,
+    input [7:0] OR,
+    input [9:0] SUM,
+    input [7:0] DIF,
+    output [9:0] ans
     );
+    
+endmodule
+
+module XOR (
+    input [7:0] num1,
+    input [7:0] num2,
+    output [7:0] ans
+    );
+    assign ans[0] = num1[0] ^ num2[0];
+    assign ans[1] = num1[1] ^ num2[1];
+    assign ans[2] = num1[2] ^ num2[2];
+    assign ans[3] = num1[3] ^ num2[3];
+    assign ans[4] = num1[4] ^ num2[4];
+    assign ans[5] = num1[5] ^ num2[5];
+    assign ans[6] = num1[6] ^ num2[6];
+    assign ans[7] = num1[7] ^ num2[7];
+    
 endmodule
